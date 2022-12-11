187b6e3c7f94c73215ef914e0123e4384d95ac36
Blissling <unconfigured@null.spigotmc.org>
2022-12-11 14:33:16 +0100

    Added ball shooting function, added default ball constructor

--- Math Seminar 2/Ball.cs
+++ Math Seminar 2/Ball.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,30 +11,53 @@ namespace Math_Seminar_2
 {
     internal class Ball
     {
+        MouseState mouseState;
+        MouseState previousMouseState;
+
         Texture2D texture;
         Vector2 position;
-        Vector2 velocity; // angle determined by mouse position?
+        Vector2 direction; // angle determined by mouse position?
+        float speed;
         float scale;
         bool fired;
 
-        public Ball(Texture2D texture, Vector2 velocity, float radius)
+        public Ball(Texture2D texture, float speed, float radius)
         {
             this.texture = texture;
             this.position = new Vector2(0, Game1.windowSize.Y-this.texture.Height);
-            this.velocity = velocity;
+            this.speed = speed;
             this.scale = radius / (texture.Width / 2);
+            this.fired = false;
+        }
+        
+        // Make a ball with default variable values
+        public Ball(Texture2D texture)
+        {
+            this.texture = texture;
+            this.position = new Vector2(0, Game1.windowSize.Y-this.texture.Height);
+            this.speed = 400f;
+            this.scale = 1;
             this.fired = false;
         }
 
         public void Update(GameTime gameTime)
         {
+            previousMouseState = mouseState;
+            mouseState = Mouse.GetState();
+
             if (fired)
-                position += velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                position += direction * speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            else
+            {
+                direction = Vector2.Normalize(mouseState.Position.ToVector2() - position);
+                if (mouseState.LeftButton == ButtonState.Pressed && previousMouseState.LeftButton == ButtonState.Released)
+                    fired = !fired;
+            }
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(texture, position, null, Color.White, 0, Vector2.Zero, scale, SpriteEffects.None, 0);
+            spriteBatch.Draw(texture, position + new Vector2(texture.Width/2, texture.Height/2), null, Color.White, 0, new Vector2(texture.Width/2, texture.Height/2), scale, SpriteEffects.None, 0);
         }
     }
 }
