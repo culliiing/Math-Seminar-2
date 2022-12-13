500c537a01021e88846a2e4383d9d69f0ea4a9c7
EdgarSjoberg <edgar.sjoberg@gmail.com>
2022-12-13 18:20:35 +0100

    Revert "adds rotation"
    This reverts commit 3836447704002dff5f41443849c534f580db4152.

--- Math Seminar 2/Car.cs
+++ Math Seminar 2/Car.cs
@@ -8,37 +8,26 @@ using Spline;
 using Microsoft.Xna.Framework.Graphics;
 using System.IO;
 using SharpDX.Direct3D9;
-using System.Diagnostics;
 
 namespace Math_Seminar_2
 {
     internal class Car // or class for path on which travelling objects can be made
     {
-
-        float maxAngle = 90f;
-
         Vector2 velocity; // vinkelhastiget?
 
         Vector2 origin;
-
-        Vector2 distanceVector;
 
         float speed = 3;
 
         float carPos;
 
-        float angle;
-
-        Vector2 lastPos, currentPos;
-
-        float rotation = 1.1f;
+        float rotation = 0;
 
         bool movingForward = true;
 
         Texture2D carTex;
 
         SimplePath path;
-        
 
         public Car(Texture2D carTex, SimplePath path)
         {
@@ -52,9 +41,6 @@ namespace Math_Seminar_2
 
         public void Update()
         {
-
-            lastPos = path.GetPos(carPos); //takes last position to calculate direction
-            
             if(carPos > path.endT && movingForward)
             {
                 movingForward = false;
@@ -72,36 +58,6 @@ namespace Math_Seminar_2
             {
                 carPos -= speed;
             }
-
-            currentPos = path.GetPos(carPos); //takes current posistion to calculate 
-
-            if (movingForward)
-            {
-                 distanceVector = new Vector2(currentPos.X - lastPos.X, currentPos.Y - lastPos.Y);
-            }
-            if(!movingForward)
-            {
-                 distanceVector = new Vector2(lastPos.X - currentPos.X, lastPos.Y - currentPos.Y);
-            }
-
-            angle = (float)Math.Atan2(distanceVector.Y - 0, distanceVector.X - 1);
-
-            angle = MathHelper.ToDegrees(angle);
-
-            if (angle >= maxAngle)
-            {
-                angle = maxAngle;
-            }
-
-            if (angle <= -maxAngle)
-            {
-                angle = -maxAngle;
-            }
-
-            Debug.WriteLine(angle.ToString());
-
-            rotation = MathHelper.ToRadians(angle);
-            
         }
 
         private void SetPath()
@@ -111,12 +67,11 @@ namespace Math_Seminar_2
 
             path.AddPoint(new Vector2(0, 100));
             path.AddPoint(new Vector2(180, 400));
-            path.AddPoint(new Vector2(400, 100));
-            path.AddPoint(new Vector2(540, 250));
+            path.AddPoint(new Vector2(360, 100));
+            path.AddPoint(new Vector2(540, 400));
             path.AddPoint(new Vector2(720, 100));
-            path.AddPoint(new Vector2(800, 600));
+            path.AddPoint(new Vector2(900, 400));
             path.AddPoint(new Vector2(1080, 100));
-
         }
 
         public void Draw(SpriteBatch spriteBatch)
